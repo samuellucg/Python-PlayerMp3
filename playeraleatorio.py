ac5ea56110c9e2cec7ee864c9b518c9007b8868b
samuellucg <98235089+samuellucg@users.noreply.github.com>
2024-05-29 23:30:40 -0300

    playeraleatorio.py

--- playeraleatorio.py
+++ playeraleatorio.py
@@ -2,7 +2,9 @@ import pygame
 import random
 print("Boa noite, temos 2 músicas para você ouvir hoje!")
 print("Qual você deseja ouvir hoje?")
+#música de sua escolha, abaixo apenas um exemplo
 music = str(input("1 - Heart_Freestyle \n2 - Pink and White\n3 - Aleatório ")).strip()
+#playlist com apenas 2 músicas, mas deve-se colocar todas que pretender usar.
 playlist = [1,2]
 if music == ("1"):
     pygame.init()
